543545a0cc5a80f46cfa26ce2ccde99e12f4e63a
jediscript <inboxjedi@gmail.com>
2023-10-04 15:55:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
 
 function App() {
+  const onSearch = () => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className='App'>
+      <Box className='App-header' > Search InDiGo</Box>
+      <Box  className='App-content' p={20}>
+        <TextField 
+        className='App-searchBox'
+        fullWidth 
+        id="fullWidth" 
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}/>
+        <Button 
+        className='App-searchButton'
+        variant='outlined' onClick={onSearch} > Search </Button>
+      </Box>
+      <Box className='App-footer'> </Box>
+    </Box>
   );
 }
 
